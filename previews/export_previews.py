ad2134a1df999807a2e6b6342e82f58e838251d9
hayk <haykh.astro@gmail.com>
2023-03-04 16:45:48 -0500

    :fire: previews upd

--- previews/export_previews.py
+++ previews/export_previews.py
@@ -5,18 +5,25 @@ import matplotlib.pyplot as plt
 import myplotlib
 import myplotlib.tests as mypltests
 
-readme = ""
+if __name__ == "__main__":
+    readme = ""
 
-for st, fl in [
-    f.replace(".mplstyle", "").split(".")
-    for f in os.listdir("myplotlib/assets")
-    if f.endswith(".mplstyle")
-]:
+    for st, fl in [
+        f.replace(".mplstyle", "").split(".")
+        for f in os.listdir("myplotlib/assets")
+        if f.endswith(".mplstyle")
+    ]:
+        mpl.rcParams.update(mpl.rcParamsDefault)
+        myplotlib.load(st, fl)
+        mypltests.testAll()
+        plt.savefig(f"previews/{st}_{fl}.jpg")
+        readme += f"# `{st}.{fl}`\n\n![{st}_{fl}]({st}_{fl}.jpg)\n\n"
+
     mpl.rcParams.update(mpl.rcParamsDefault)
-    myplotlib.load(st, fl)
+    myplotlib.load(None, None)
     mypltests.testAll()
-    plt.savefig(f"previews/{st}_{fl}.jpg")
-    readme += f"# `{st}.{fl}`\n\n![{st}_{fl}]({st}_{fl}.jpg)\n\n"
+    plt.savefig(f"previews/None.jpg")
+    readme += f"# `None`\n\n![None](None.jpg)\n\n"
 
-with open("previews/README.md", "w") as f:
-    f.write(readme)
+    with open("previews/README.md", "w") as f:
+        f.write(readme)
